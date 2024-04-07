e0b28efe5c9a7ef444fc7c1572fc871f806a6192
Nitin Chava <nitinchava10@gmail.com>
2024-04-07 10:40:34 -0500

    finished court line detection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
+from court_line_detector import CourtLineDetector
 
 def main():
     #reads the video
@@ -13,9 +14,15 @@ def main():
     ball_tracker = BallTracker(model_path='models/yolo5_last.pt')
     ball_detections = ball_tracker.detect_frames(video_frames, read_from_remain=True, remain_path="tracker_remains/ball_detections.pkl")
 
-    #drawing output of player bounding boxes & ball bounding boxes
+    #court line detection
+    court_model_path = "models/keypoints_model.pth"
+    court_line_detector = CourtLineDetector(court_model_path)
+    court_keypoints = court_line_detector.predict(video_frames[0])
+
+    #drawing output of player bounding boxes & ball bounding boxes & court keypoints
     output_video_frames = player_tracker.draw_boundingboxes(video_frames, player_detections)
-    output_video_frames = ball_tracker.draw_boundingboxes(video_frames, ball_detections)
+    output_video_frames = ball_tracker.draw_boundingboxes(output_video_frames, ball_detections)
+    output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
 
     save_video(output_video_frames, "output_videos/output_video.avi")
 
