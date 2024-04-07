45e9df92502135de6cea84579796d1d7256e24b0
Nitin Chava <nitinchava10@gmail.com>
2024-04-07 10:56:23 -0500

    fixed deprecated function error

--- court_line_detector/court_line_detector.py
+++ court_line_detector/court_line_detector.py
@@ -6,7 +6,8 @@ import numpy as np
 
 class CourtLineDetector:
     def __init__(self, model_path):
-        self.model = models.resnet50(pretrained=True)
+        self.model = models.resnet50(weights='ResNet50_Weights.IMAGENET1K_V1')
+        #self.model = models.resnet50(pretrained=True)
         self.model.fc = torch.nn.Linear(self.model.fc.in_features, 14*2)
         self.model.load_state_dict(torch.load(model_path, map_location='cpu'))
 
