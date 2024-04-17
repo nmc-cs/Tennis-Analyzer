c13c6cb8f3f3b4a0836c0384ce8d9a3bc7f65c02
Nitin Chava <nitinchava10@gmail.com>
2024-04-17 16:37:40 -0500

    drew court lines and net

--- mini_court_map/mini_court_map.py
+++ mini_court_map/mini_court_map.py
@@ -110,11 +110,22 @@ class MiniCourt():
         self.start_x = self.end_x - self.drawing_rectangle_width
         self.start_y = self.end_y - self.drawing_rectangle_height
 
-    def draw_court(self, frame):
+    def draw_court(self,frame):
         for i in range(0, len(self.drawing_key_points),2):
             x = int(self.drawing_key_points[i])
             y = int(self.drawing_key_points[i+1])
-            cv2.circle(frame, (x,y),5, (0,0,255), -1)
+            cv2.circle(frame, (x,y),5, (0,0,255),-1)
+
+        # draw Lines
+        for line in self.lines:
+            start_point = (int(self.drawing_key_points[line[0]*2]), int(self.drawing_key_points[line[0]*2+1]))
+            end_point = (int(self.drawing_key_points[line[1]*2]), int(self.drawing_key_points[line[1]*2+1]))
+            cv2.line(frame, start_point, end_point, (0, 0, 0), 2)
+
+        # Draw net
+        net_start_point = (self.drawing_key_points[0], int((self.drawing_key_points[1] + self.drawing_key_points[5])/2))
+        net_end_point = (self.drawing_key_points[2], int((self.drawing_key_points[1] + self.drawing_key_points[5])/2))
+        cv2.line(frame, net_start_point, net_end_point, (255, 0, 0), 2)
 
         return frame
 
